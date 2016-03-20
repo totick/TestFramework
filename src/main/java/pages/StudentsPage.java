d99486e4e1267313f5a86f62cb08e2889c3a5bf9
John Lundgren <john.paul.lundgren@gmail.com>
2016-03-20 16:32:37 +0100

    Added functionality to the StudentPage

--- src/main/java/pages/StudentsPage.java
+++ src/main/java/pages/StudentsPage.java
@@ -1,5 +1,9 @@
 package pages;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -12,24 +16,27 @@ public class StudentsPage {
 	private static final String TITLE = "Students";
 	@FindBy(how = How.CSS, using="html body div.container div#content div.container.body-content div.row div.col-xs-10.col-lg-12 div.row div.col-lg-6.col-md-6 fieldset button.btn.btn-success")
 	private WebElement showStudentFormButton;
-	@FindBy(how = How.ID, using = "newFirstName")
+	@FindBy(how = How.ID, using = "asf:newFirstName")
 	private WebElement newFirstNameField;
-	@FindBy(how = How.ID, using = "newLastName")
+	@FindBy(how = How.ID, using = "asf:newLastName")
 	private WebElement newLastNameField;
-	@FindBy(how = How.ID, using = "newPersonnr")
+	@FindBy(how = How.ID, using = "asf:newPersonnr")
 	private WebElement newPersonnrField;
-	@FindBy(how = How.ID, using = "newGender")
-	private WebElement newGenderRb;
-	@FindBy(how = How.ID, using = "newStreet")
+	@FindBy(how = How.NAME, using = "asf:newGender")
+	private List<WebElement> newGenderRb;
+	@FindBy(how = How.ID, using = "asf:newStreet")
 	private WebElement newStreetField;
-	@FindBy(how = How.ID, using = "newCity")
+	@FindBy(how = How.ID, using = "asf:newCity")
 	private WebElement newCityField;
-	@FindBy(how = How.ID, using = "newTelephone")
+	@FindBy(how = How.ID, using = "asf:newTelephone")
 	private WebElement newTelephoneField;
-	@FindBy(how = How.ID, using = "newEmail")
+	@FindBy(how = How.ID, using = "asf:newEmail")
 	private WebElement newEmailField;
-	@FindBy(how = How.ID, using = "newPassword")
+	@FindBy(how = How.ID, using = "asf:newPassword")
 	private WebElement newPasswordField;
+	@FindBy(how = How.ID, using = "asf:studentFormSubmit")
+	private WebElement submitNewStudentForm;
+	
 	
 	public void goTo() {
 		Browser.goTo(URL);
@@ -41,10 +48,61 @@ public class StudentsPage {
 	
 	public void showAddNewStudentForm(){
 		showStudentFormButton.click();
+		Browser.getDriver().manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 	}
 	
-	public void addValueToNewFirstNameField(){
-		
+	public void addValueToNewFirstNameField(String value){
+		this.newFirstNameField.sendKeys(value);
 	}
 	
+	public void addValueToNewLastNameField(String value){
+		this.newLastNameField.sendKeys(value);
+	}
+	
+	public void addValueToNewPersonnrField(String value){
+		this.newPersonnrField.sendKeys(value);
+	}
+	
+	public void addValueToNewGenderRb(String value){
+		if(value.equals("male")){
+			this.newGenderRb.get(0).click();
+		}
+		else{
+			this.newGenderRb.get(1).click();
+		}
+	}
+	
+	public void addValueToNewStreetField(String value){
+		this.newStreetField.sendKeys(value);
+	}
+	
+	public void addValueToNewCityField(String value){
+		this.newCityField.sendKeys(value);
+	}
+	
+	public void addValueToNewTelephoneField(String value){
+		this.newTelephoneField.sendKeys(value);
+	}
+	
+	public void addValueToNewEmailField(String value){
+		this.newEmailField.sendKeys(value);
+	}
+	
+	public void addValueToNewPasswordField(String value){
+		this.newPasswordField.sendKeys(value);
+	}
+	
+	public void submitAddStudentForm(){
+		this.submitNewStudentForm.click();
+	}
+	
+	public boolean isTextInStudentsListTable(String text){
+		List<WebElement> elements = Browser.getDriver().findElements(By.cssSelector("#sf\\:studentListTable td"));
+		for(WebElement elem : elements){
+			if(elem.getText().trim().equals(text)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
---
+++ src/main/java/wrappers/StudentWrapper.java
@@ -0,0 +1,71 @@
+package wrappers;
+
+public class StudentWrapper {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String personnr;
+	//private String gender;
+	private String street;
+	private String city;
+	private String telephone;
+	private String email;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPersonnr() {
+		return personnr;
+	}
+	public void setPersonnr(String personnr) {
+		this.personnr = personnr;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
